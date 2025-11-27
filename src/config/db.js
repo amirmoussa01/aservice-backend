92e325f04d5bf6ffca4a8cb0152bfc4fafe02233
amirmoussa01 <moussaamir979@gmail.com>
2025-11-27 11:02:47 +0100

    modif3

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.PORT || 3306,
   connectionLimit: 10,
