d76a1d2c6a50c7cc2215ee3b14dd8c61aac1d61f
amirmoussa01 <moussaamir979@gmail.com>
2025-12-26 07:44:44 -0800

    Services modif

--- src/routes/service.routes.js
+++ src/routes/service.routes.js
@@ -7,15 +7,15 @@ import {
   getPublicServices,
   getServicesByCategory
 } from "../controllers/service.controller.js";
-import { authenticateToken } from "../middlewares/auth.js";
-
+import { auth } from "../middlewares/auth.js";
+import { isProvider } from "../middlewares/roles.js";
 const router = express.Router();
-
+router.use(auth, isProvider);
 // Routes prestataire (protégées)
-router.post("/provider/services", authenticateToken, createService);
-router.get("/provider/services", authenticateToken, getMyServices);
-router.put("/provider/services/:id", authenticateToken, updateService);
-router.delete("/provider/services/:id", authenticateToken, deleteService);
+router.post("/provider/services", createService);
+router.get("/provider/services", getMyServices);
+router.put("/provider/services/:id", updateService);
+router.delete("/provider/services/:id", deleteService);
 
 // Routes publiques
 router.get("/services", getPublicServices);
